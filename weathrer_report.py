ea5e54b4f05bd03083283b39ae155f5f0e20c520
yukichawan <117551054+yukichawan@users.noreply.github.com>
2023-02-05 16:34:31 +0900

    Update weathrer_report.py

--- weathrer_report.py
+++ weathrer_report.py
@@ -1,6 +1,6 @@
 import requests #pip install requestsなどが必要　HTTPリクエストを簡単に行うためのものです
 
-#GETメソッドによる天気予報APIを使用し土浦市の今日の天気予報を取得するプログラム
+#GETメソッドによる天気予報APIを使用し那覇市の今日の天気予報を取得するプログラム
 class TuchiuraWeather(object):
     def get_todays_forecast(self):
         try:#今日の天気予報を取得し、その結果を返します
@@ -10,10 +10,10 @@ class TuchiuraWeather(object):
     
     def _fetch(self):#天気予報APIに対してHTTP GETリクエストを送信し、JSON形式で受け取った結果を返します。
         return requests.\
-            get('https://weather.tsukumijima.net/api/forecast/city/080020').\
+            get('https://weather.tsukumijima.net/api/forecast/city/471010').\
             json()
 
 if __name__ == '__main__':
-    print("土浦の今日の天気予報は：")
+    print("那覇の今日の天気予報は：")
     #TuchiuraWeatherのインスタンスを作成し、get_todays_forecast()メソッドを呼び出して、土浦市の今日の天気予報を取得し、出力します。
     print(TuchiuraWeather().get_todays_forecast())
